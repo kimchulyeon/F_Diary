8239a7637e658d8f760b39d8b32f7846f4e53e85
chulyeon <guinness987@gmail.com>
2023-03-06 15:55:54 +0900

    Initial Commit

---
+++ F_Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  F_Diary
+//
+//  Created by chulyeon kim on 2023/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
